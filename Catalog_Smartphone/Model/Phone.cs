567dd489b56bcde89b8e085d9d6509e82f8f255e
Dimon <Dimon@DESKTOP-RMP3SSA>
2018-10-19 22:31:59 +0300

    1.0

---
+++ Catalog_Smartphone/Model/Phone.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace Catalog_Smartphone
+{
+    public class Phone
+    {
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public double Price { get; set; }
+        public string Image { get; set; }
+        public static ObservableCollection<Phone> GetAll()
+        {
+            return new ObservableCollection<Phone>()
+            {
+                new Phone{ Manufacturer="Xiaomi", Model="Redmi note 5", Price=4899, Image = "../Images/Phone.ico"},
+                new Phone{ Manufacturer="Samsung", Model="J5", Price=5499, Image = "../Images/Phone.ico" }
+            };
+        }
+    }
+}
